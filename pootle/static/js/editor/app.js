8ad19bc0ce6c8543a61ce67b3fdb923f08b6129b
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-05-18 17:44:00 +0200

    RawFontTextarea: consolidate all logic in a single component
    This commit puts all raw font logic in a single component, which moves the state
    away to parent components, simplifies the overall component tree and also allows
    to get rid of some part of the ugly code being used in order to allow the
    DOM-based editor to deal with the React-based editor state.
    
    Setting values as props from the outside world has also been made more explicit,
    by accepting an `overrideValues` prop instead of `values`, which clearly exposes
    the intent and the expected side-effect.
    
    The remaining not-so-beautifl parts have been explicitly commented in the code
    to allow an easy removal when the whole editor state is managed via a
    components.

--- pootle/static/js/editor/app.js
+++ pootle/static/js/editor/app.js
@@ -705,7 +705,7 @@ PTL.editor = {
       start = $target.caret().start + text.length;
     }
 
-    ReactEditor.setProps({ values: newValues });
+    ReactEditor.setProps({ overrideValues: newValues });
     $target.caret(start, start);
   },
 
@@ -713,7 +713,7 @@ PTL.editor = {
   /* Copies source text(s) into the target textarea(s)*/
   copyOriginal(languageCode) {
     const sources = this.units.getCurrent().get('sources')[languageCode];
-    ReactEditor.setProps({ values: sources.slice() });
+    ReactEditor.setProps({ overrideValues: sources.slice() });
 
     this.goFuzzy();
 
@@ -2367,7 +2367,7 @@ PTL.editor = {
 
   processAcceptSuggestion(data, suggId, skipToNext) {
     if (data.newtargets !== undefined) {
-      ReactEditor.setProps({ values: data.newtargets.slice() });
+      ReactEditor.setProps({ overrideValues: data.newtargets.slice() });
     }
     this.focused.focus();
     this.updateUnitDefaultProperties();
@@ -2441,7 +2441,7 @@ PTL.editor = {
     }
 
     ReactEditor.setProps({
-      values: this.updateFocusedValue(decodeEntities(translation)),
+      overrideValues: this.updateFocusedValue(decodeEntities(translation)),
     });
 
     const area = this.focused;
