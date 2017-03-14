b7807ac1e08ad222064e054359d27466c12a910c
Taras Semenenko <taras.semenenko@gmail.com>
2017-03-14 18:47:09 +0300

    WIP add Score data update implementation

--- pootle/apps/pootle_score/receivers.py
+++ pootle/apps/pootle_score/receivers.py
@@ -6,16 +6,42 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+import logging
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from pootle_statistics.models import ScoreLog, TranslationActionCodes
+from pootle.core.signals import update_score_data
+from pootle_statistics.models import Submission, ScoreLog, TranslationActionCodes
+from pootle_store.models import Suggestion, UnitSource, Store
+
+from .models import UserStoreScore
 
 
-@receiver(post_save, sender=ScoreLog)
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_save, sender=UserStoreScore)
+def handle_tp_score_data_save(**kwargs):
+    tp = kwargs["instance"].store.translation_project
+    update_score_data.send(tp.__class__, instance=tp)
+
+
+@receiver(post_save, sender=Submission)
+def handle_store_score_data_on_submission_save(**kwargs):
+    submission = kwargs["instance"].unit.store
+    update_score_data.send(submission.__class__, instance=submission)
+
+
+@receiver(post_save, sender=Suggestion)
+def handle_store_score_data_on_suggestion_save(**kwargs):
+    suggestion = kwargs["instance"]
+    update_score_data.send(suggestion.__class__, instance=suggestion)
+
+
 def handle_scorelog_save(**kwargs):
     scorelog = kwargs["instance"]
-    tp = scorelog.submission.translation_project
+    store = scorelog.submission.unit.store
     created = False
     changed = dict(suggested=0, translated=0, reviewed=0)
     review_actions = [
@@ -27,11 +53,11 @@ def handle_scorelog_save(**kwargs):
     elif scorelog.action_code in review_actions:
         changed["reviewed"] = scorelog.wordcount
     try:
-        user_score = tp.user_scores.get(
+        user_score = store.user_scores.get(
             date=scorelog.creation_time.date(),
             user=scorelog.user)
-    except tp.user_scores.model.DoesNotExist:
-        user_score = tp.user_scores.create(
+    except store.user_scores.model.DoesNotExist:
+        user_score = store.user_scores.create(
             date=scorelog.creation_time.date(),
             user=scorelog.user,
             score=scorelog.score_delta,
